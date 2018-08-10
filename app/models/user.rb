183c08fbef9a56ff1487de7097ad73cc8e33d37a
Zac Willmington <zacwillmington@Zacs-MacBook-Air.local>
2018-08-10 10:00:24 -0700

    Add validation to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :apartments, through: :appointments
     has_many :apartments
     has_secure_password
+
+    validates :name, :presence => true
+    validates :email, :presence => true
+    validates :password, :presence => true
+
+    validates :email, inclusion: { in: %w(.com), message: 'Must be valid email' }
 end
