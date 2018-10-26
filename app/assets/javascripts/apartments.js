432d967d2b933030602f42e98b70a9c7682d9b74
github username <github email address>
2018-10-26 16:52:25 -0700

    Add attachListeners to listen for click event to show all apartments.

--- app/assets/javascripts/apartments.js
+++ app/assets/javascripts/apartments.js
@@ -1,7 +1,18 @@
-$( document ).ready(function(){
+$( document ).ready(function() {
     attachListeners();
 });
 
-function attachListeners(){
-    
+
+function attachListeners() {
+    $('.js-get-all-apartments-btn').on('click', function(e) {
+        e.preventDefault();
+         getApartments(e);
+    });
+}
+
+function getApartments(e) {
+    $.get(`${e.target.href}`).done(function(data) {
+        debugger;
+        // get data and insert into users/show page
+    });
 }
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
       resources :appointments
   end
 
-  resources :apartments
+  # resources :apartments
 
-  
+
 
   get '/past_appointments' => 'apartments#past_appointments'
 
