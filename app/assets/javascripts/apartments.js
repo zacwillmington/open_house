bb69d52aa80a5265b6466f4f1c466daee7d808f1
github username <github email address>
2018-10-27 15:54:16 -0700

    create ES6 classes for Apartments, Appointments and Users. Grab data from API and instanciate objects in getApartment() function.

--- app/assets/javascripts/apartments.js
+++ app/assets/javascripts/apartments.js
@@ -1,7 +1,17 @@
-$( document ).ready(function() {
-    attachListeners();
-});
+$( document ).ready( () => attachListeners());
 
+//ES6 classes
+class Apartment {
+    constructor(id, address, available_times, image, bedrooms, bathrooms, parking, price) {
+        self.address = address;
+        self.available_times = available_times;
+        self.image = image;
+        self.bedrooms = bedrooms;
+        self.bathrooms = bathrooms;
+        self.parking = parking;
+        self.price = price;
+    }
+}
 
 function attachListeners() {
     $('.js-get-all-apartments-btn').on('click', function(e) {
@@ -12,10 +22,24 @@ function attachListeners() {
 
 function getApartments(e) {
     $.get(`${e.target.href}`).done(function(data) {
-        debugger;
-        // ES6 create class for each apartment. Then
-        // addToUsersShow(data);
+        // ES6 create class for each apartment.
+        data.forEach((apartment) => {
+            apt = new Apartment;
+            apt.id = apartment.id;
+            apt.address = apartment.address;
+            apt.available_times = apartment.available_times;
+            apt.image = apartment.image;
+            apt.bedrooms = apartment.bedrooms;
+            apt.bathrooms = apartment.bathrooms;
+            apt.parking = apartment.parking;
+            apt.price = apartment.price;
+            debugger;
+            //push to where?
+        });
+        // addApartmentsToUsersShow(data);
     });
 }
 
-function
+function addApartmentsToUsersShow(apartments) {
+
+}
---
+++ app/assets/javascripts/appointments.js
@@ -0,0 +1,8 @@
+class Appointment {
+    constructor(time, user_id, apartment_id, name) {
+            self.time = time;
+            self.user_id = user_id;
+            self.apartment_id = apartment_id;
+            self.name = name;
+    }
+}
