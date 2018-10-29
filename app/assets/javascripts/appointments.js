1a6221700fd8e6994a899104ec36b29a5bb32ca7
github username <github email address>
2018-10-29 09:56:03 -0700

    Coded out createApartments() and createAppointments() functions.

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,8 +1,23 @@
 class Appointment {
-    constructor(time, user_id, apartment_id, name) {
+    constructor(id, time, user_id, apartment_id, name) {
+            self.id = id;
             self.time = time;
             self.user_id = user_id;
             self.apartment_id = apartment_id;
             self.name = name;
     }
+}
+
+function createAppointments(appointments) {
+    let appointmentsArray = [];
+    appointments.forEach( (app) => {
+        appointment = new Appointment;
+        appointment.id = app.id;
+        appointment.time = app.time;
+        appointment.user_id = app.user_id;
+        appointment.apartment_id = app.apartment_id;
+        appointment.name = app.name;
+        appointmentsArray.push(appointment);
+    });
+    return appointmentsArray;
 }
